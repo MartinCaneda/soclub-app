37cc123a53acf675defcb17d0cf1a9e814b5068a
Martin <martincanedagz@gmail.com>
2024-04-12 10:43:29 +0200

    fist commit

--- soclub-app/src/components/NavBar/index.js
+++ soclub-app/src/components/NavBar/index.js
@@ -3,18 +3,20 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <nav>
-      <li>
-        <Link href="../Events">Events</Link>
-      </li>
-      <li>
-        <Link href="../MyEvents">My Events</Link>
-      </li>
-      <li>
-        <Link href="../ProfilePage">My profile</Link>
-      </li>
-      <li>
-        <Link href="../map">Map</Link>
-      </li>
+      <ul>
+        <li>
+          <Link href="/events">Events</Link>
+        </li>
+        <li>
+          <Link href="/MyEvents">My Events</Link>
+        </li>
+        <li>
+          <Link href="/ProfilePage">My profile</Link>
+        </li>
+        <li>
+          <Link href="/Map">Map</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
