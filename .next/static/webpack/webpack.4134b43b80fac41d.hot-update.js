92f77a283bf8ff749b9bab9ad4cc22460b2acad3
Martin <martincanedagz@gmail.com>
2024-04-16 14:31:03 +0200

    create map page, render map in map page, tweak layout

--- .next/static/webpack/webpack.4134b43b80fac41d.hot-update.js
+++ .next/static/webpack/webpack.4134b43b80fac41d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3f35ddd6ba609fea"; }
+/******/ 	__webpack_require__.h = function() { return "c8365149b0d166e9"; }
 /******/ }();
 /******/ 
 /******/ }
---
+++ src/pages/map/index.js
@@ -0,0 +1,11 @@
+import MyMap from "@/components/Map";
+import Map from "react-map-gl/dist/esm/components/map";
+
+export default function MapPage() {
+  return (
+    <>
+      <h2>Map</h2>
+      <MyMap />
+    </>
+  );
+}
