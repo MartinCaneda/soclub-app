e6564876b66c8da670916d822425a22ebd69b134
Martin <martincanedagz@gmail.com>
2024-04-16 14:52:21 +0200

    to lower case

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -4,7 +4,7 @@ import Map from "react-map-gl/dist/esm/components/map";
 export default function MapPage() {
   return (
     <>
-      <h2>Map</h2>
+      <h2>map</h2>
       <MyMap />
     </>
   );
