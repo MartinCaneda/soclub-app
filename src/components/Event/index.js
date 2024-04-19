0dd29eda798a48dc2268570d841da785344e271a
Martin <martincanedagz@gmail.com>
2024-04-19 10:34:35 +0200

    the delete works, gotta narrow it down to only be able to delete events created by the current user

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -1,12 +1,16 @@
 import useSWR from "swr";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import AddEventForm from "../AddEventForm";
 
 export default function Event() {
   const router = useRouter();
   const { id } = router.query;
 
+  const { mutate } = useSWR("/api/events");
+  const [isEditMode, setIsEditMode] = useState(false);
   const { data, isLoading } = useSWR(`/api/events/${id}`);
-  console.log("data", data);
+
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
@@ -14,12 +18,48 @@ export default function Event() {
   if (!data) {
     return;
   }
+  async function handleEditProduct(event) {
+    event.preventDefault();
 
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+    const response = await fetch(`/api/events/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+    }
+  }
+  async function handleDeleteEvent() {
+    const response = await fetch(`/api/events/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      router.push("/");
+    }
+  }
   return (
     <>
+      {isEditMode && <AddEventForm onSubmit={handleEditProduct} />}
       <h2>Event: {data.name}</h2>
       <p>Location: {data.location}</p>
       <p>Description: {data.description}</p>
+      <button
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        ✏️
+      </button>
+      <button type="button" onClick={() => handleDeleteEvent()}>
+        ❌
+      </button>
     </>
   );
 }
--- src/pages/api/events/[id]/index.js
+++ src/pages/api/events/[id]/index.js
@@ -13,4 +13,13 @@ export default async function handler(request, response) {
       return response.status(404).json({ status: "Not Found" });
     }
   }
+  if (request.method === "PUT") {
+    const updatedEvent = request.body;
+    await Event.findByIdAndUpdate(id, updatedEvent);
+    response.status(200).json({ status: `Event ${id} successfully updated!` });
+  }
+  if (request.method === "DELETE") {
+    await Event.findByIdAndDelete(id);
+    response.status(200).json({ status: "Event successfully deleted" });
+  }
 }
