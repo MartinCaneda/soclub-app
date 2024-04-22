79baf0b7986f06c69630b9d1e548ada95ae2c354
Martin <martincanedagz@gmail.com>
2024-04-22 10:14:26 +0200

    switch to a different branch

---
+++ src/components/FavoriteButton/index.js
@@ -0,0 +1,17 @@
+export default function FavoriteButton({
+  artPiecesInfo,
+  onToggleFavorite,
+  slug,
+}) {
+  const foundArtPieceInfo = artPiecesInfo.find((info) => {
+    return info.slug === slug;
+  });
+
+  return (
+    <button className="favButton" onClick={() => onToggleFavorite(slug)}>
+      <span>
+        {foundArtPieceInfo?.isFavorite ? "❤️" : <i>Add to Favorites</i>}
+      </span>
+    </button>
+  );
+}
