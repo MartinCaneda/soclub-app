68b36a86853eb224e44d77fb89c1773c3d344668
Martin <martincanedagz@gmail.com>
2024-04-24 14:54:21 +0200

    add join button, saved user on event colleciton property

--- src/pages/api/events/[id]/index.js
+++ src/pages/api/events/[id]/index.js
@@ -1,24 +1,41 @@
-import dbConnect from "@/db/connect";
-import Event from "@/db/models/Event";
+import dbConnect from "@/db/connect"
+import Event from "@/db/models/Event"
 
 export default async function handler(request, response) {
-  await dbConnect();
-  const { id } = request.query;
+  await dbConnect()
+  const { id } = request.query
 
   if (request.method === "GET") {
-    const event = await Event.findById(id);
+    const event = await Event.findById(id)
     if (!event) {
-      return response.status(404).json({ status: "Not Found" });
+      return response.status(404).json({ status: "Not Found" })
     }
-    response.status(200).json(event);
+    response.status(200).json(event)
   }
   if (request.method === "PUT") {
-    const updatedEvent = request.body;
-    await Event.findByIdAndUpdate(id, updatedEvent);
-    response.status(200).json({ status: `Event ${id} successfully updated!` });
+    const updatedEvent = request.body
+    await Event.findByIdAndUpdate(id, updatedEvent)
+    response.status(200).json({ status: `Event ${id} successfully updated!` })
   }
   if (request.method === "DELETE") {
-    await Event.findByIdAndDelete(id);
-    response.status(200).json({ status: "Event successfully deleted" });
+    await Event.findByIdAndDelete(id)
+    response.status(200).json({ status: "Event successfully deleted" })
+  }
+  if (request.method === "POST") {
+    const { userId } = request.body
+    try {
+      const event = await Event.findById(id)
+      event.participants.push(userId)
+      await event.save()
+
+      if (!event) {
+        return response.status(404).json({ status: "Not Found" })
+      }
+
+      response.status(200).json({ status: `User ${userId} joined event ${id}` })
+    } catch (error) {
+      console.error(error)
+      response.status(500).json({ status: "Internal Server Error" })
+    }
   }
 }
