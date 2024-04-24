a758be7cfce7e8ea2984bea9652a87e34e2e8e61
Martin <martincanedagz@gmail.com>
2024-04-24 11:51:40 +0200

    secret

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import clientPromise from "@/db/mongodb";
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "@/db/mongodb"
 
 export const authOptions = {
   //I can add more later, keep it simple for now
@@ -16,10 +16,11 @@ export const authOptions = {
 
   callbacks: {
     async session({ session, user }) {
-      session.user.userId = user.id;
-      return session;
+      session.user.userId = user.id
+      return session
     },
   },
-};
+  secret: process.env.SECRET,
+}
 
-export default NextAuth(authOptions);
+export default NextAuth(authOptions)
