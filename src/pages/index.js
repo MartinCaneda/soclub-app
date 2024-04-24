4cd446703957ab4591b4903818e8e55711732b64
Martin <martincanedagz@gmail.com>
2024-04-24 11:35:33 +0200

    create event linked to session

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import EventList from "@/components/EventList"
 import CreateEvent from "@/components/create-event"
+import { useSession } from "next-auth/react"
 
 export default function Home() {
+  const { data: session } = useSession()
   return (
     <>
       <div>
-        <CreateEvent />
+        {session && <CreateEvent />}
         <EventList />
       </div>
     </>
