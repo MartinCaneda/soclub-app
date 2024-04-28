c8bace9184f99d28ef1f56e3c365022c4ff90743
Martin <martincanedagz@gmail.com>
2024-04-28 13:14:06 +0200

    joined events list

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,25 +1,27 @@
-import Profile from "@/components/ProfileInfo";
-import { getSession } from "next-auth/react";
-import useSWR from "swr";
-import UserEvents from "@/components/UserEvents";
+import Profile from "@/components/ProfileInfo"
+import { getSession } from "next-auth/react"
+import useSWR from "swr"
+import UserEvents from "@/components/UserEvents"
+import UserJoinedEvents from "@/components/JoinedEventsList"
 
 export default function MyProfile({ userId }) {
-  const { data, isLoading } = useSWR(`/api/user/${userId}`);
+  const { data, isLoading } = useSWR(`/api/user/${userId}`)
 
   return (
     <>
       <Profile />
       <UserEvents />
+      <UserJoinedEvents />
     </>
-  );
+  )
 }
 export async function getServerSideProps(context) {
-  const session = await getSession(context);
-  const userId = session?.user?.userId || null;
+  const session = await getSession(context)
+  const userId = session?.user?.userId || null
 
   return {
     props: {
       userId,
     },
-  };
+  }
 }
